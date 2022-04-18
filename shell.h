081aac79357ee42e46ad8c77c4f0e0862432ee24
Yasgc24 <4537@holbertonschool.com>
2022-04-18 15:06:30 -0700

    Add correction main

--- shell.h
+++ shell.h
@@ -6,8 +6,13 @@
 #include <unistd.h>
 #include <string.h>
 
+#define DELIM " \t\r\a\n"
+
 int pr_basic(void);
+char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 void free_array(char **argv, int argc);
 
 #endif
