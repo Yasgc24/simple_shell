e02a203e9c51ef04cf47b2ccd4715ef58a687680
GhostyO17 <oscar.castro9617@gmail.com>
2022-04-21 15:04:06 -0500

    Removes printf used for testing adds exit condition

--- s_sh.c
+++ s_sh.c
@@ -10,15 +10,20 @@ int main(void)
 {
 char *line = NULL;
 size_t len = 0;
-int comand;
-int argcs = 0;
+int command;
 
 while (1)
 {
-printf("$ ");
-comand = getline(&line, &len, stdin);
-argcs = argc(line);
-comand = comand;
+write(STDOUT_FILENO, "$ ", 2);
+fflush(stdin);
+command = getline(&line, &len, stdin);
+
+if (command == -1)
+{
+    free(line);
+    exit(127);
+}
+
 
 }
 return(0);
