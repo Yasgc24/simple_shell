633e162a4795ea6bf6ac411867b6aa47c60f6ed5
Yasgc24 <4537@holbertonschool.com>
2022-04-21 21:17:27 -0700

    Add betty correction

--- path.c
+++ path.c
@@ -16,37 +16,32 @@ char *add_path(char *command)
 	{
 		return (command);
 	}
-
-	i = 0;
-	while (environ[i])
+	for (i = 0; environ[i]; i++)
 	{
 		if (_strcmp(environ[i], "PATH") == 0)
-			{
-			       	path = _strcpy(path, environ[i]);
-				break;
-			}
-	i++;
-				if (path == NULL)
-				{	
-					return (NULL);
-				}
-		token = tokenizer(path, "=:");
-		
-		j = 1;
-		while (token[j])
 		{
-		       	temp = _strcpy(temp, token[j]);
-                        temp = _strcat(temp, "/");
-                        temp = _strcat(temp, command);
+			path = _strcpy(path, environ[i]);
+			break;
 		}
-			if (access(temp, X_OK) == 0)
-			{
-				free(path);
-				free(token);
-				return (temp);
-			}
-			j++;
-			free(temp);
+		if (path == NULL)
+		{
+			return (NULL);
+		}
+		token = tokenizer(path, "=:");
+		for (j = 1; token[j]; j++)
+		{
+			temp = _strcpy(temp, token[j]);
+			temp = _strcat(temp, "/");
+			temp = _strcat(temp, command);
+		}
+
+		if (access(temp, X_OK) == 0)
+		{
+			free(path);
+			free(token);
+			return (temp);
+		}
+		free(temp);
 	}
 	free(path);
 	free(token);
