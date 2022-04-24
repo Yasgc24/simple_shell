9e834fcbb1862188e57ac3446e44515c3413ee8b
Yasgc24 <4537@holbertonschool.com>
2022-04-23 19:01:22 -0700

    Add correction betty

--- path.c
+++ path.c
@@ -3,33 +3,33 @@
 /**
  * create_path_extension - creates a path extension in command
  * @cmd: command to interprate
- * @argv: tokenized arguments 
+ * @argv: tokenized arguments
  * @argc: argument counter
  *
  * Return: path with the command
  */
 char *create_path_extension(char *cmd, char **argv, int argc)
 {
-    char *environment_path = NULL, *tokenzer = NULL, *token_buf = NULL;
+	char *environment_path = NULL, *tokenzer = NULL, *token_buf = NULL;
 
-    if (cmd[0] != '/')
-    {
-        environment_path = get_path();
-        if (environment_path == NULL)
-        {
-            free_double_pointer(argv, argc);
-            perror("Error");
-        }
-        tokenzer = get_route_bin(environment_path, tokenzer);
-        token_buf = validate_access(cmd, tokenzer, argv, argc);
-        if (token_buf == NULL)
-        {
-            free_double_pointer(argv, argc);
-            perror("Error");
-        }
-        return token_buf;
-    }
-    return cmd;
+	if (cmd[0] != '/')
+	{
+		environment_path = get_path();
+		if (environment_path == NULL)
+		{
+			free_double_pointer(argv, argc);
+			perror("Error");
+		}
+		tokenzer = get_route_bin(environment_path, tokenzer);
+		token_buf = validate_access(cmd, tokenzer, argv, argc);
+		if (token_buf == NULL)
+		{
+			free_double_pointer(argv, argc);
+			perror("Error");
+		}
+		return (token_buf);
+	}
+	return (cmd);
 }
 
 /**
@@ -39,25 +39,24 @@ char *create_path_extension(char *cmd, char **argv, int argc)
  */
 char *get_path()
 {
-    char *return_path = NULL;
-    int i, path_len = 0;
+	char *ret_path = NULL;
+	int i, path_len = 0;
 
-    for (i = 0; environ[i] != NULL; i++)
-    {
-        if (_strcmp(environ[i], "PATH") == 0)
-        {
-            path_len = _strlen(environ[i]);
-            return_path = malloc(sizeof(char) * (path_len + 1));
-            if (return_path == NULL)
-            {
-                return NULL;
-            }
-            return_path = _strcpy(return_path, environ[i]);
-        }
-    }
-    return return_path;
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		if (_strcmp(environ[i], "PATH") == 0)
+		{
+			path_len = _strlen(environ[i]);
+			ret_path = malloc(sizeof(char) * (path_len + 1));
+			if (ret_path == NULL)
+			{
+				return (NULL);
+			}
+			ret_path = _strcpy(ret_path, environ[i]);
+		}
+	}
+	return (ret_path);
 }
-
 
 /**
  * get_route_bin - Splits the path from the env var to keep only the path
@@ -76,42 +75,42 @@ char *get_route_bin(char *path, char *token)
 
 /**
  * validate_access - check wheter the file exist or not in the path
- * @cmd: input command 
+ * @cmd: input command
  * @tokenizer: tokens
  * @argv: tokenized arguments
  * @argc: counter of arguments
- *
  * Return: temp if access worked - NULL otherwise
  */
-char *validate_access(char *cmd, char*tokenizer ,char **argv, int argc)
+
+char *validate_access(char *cmd, char *tokenizer, char **argv, int argc)
 {
-    int tokenizer_len = 0;
-    char *return_token = NULL;
+	int tokenizer_len = 0;
+	char *ret_token = NULL;
 
-    while (tokenizer != NULL)
-    {
-        tokenizer_len = _strlen(tokenizer);
-        return_token = malloc(sizeof(char) * (tokenizer_len + _strlen(cmd) + 2));
-        if (return_token == NULL)
-        {
-            free_double_pointer(argv, argc);
-            return NULL;
-        }
+	while (tokenizer != NULL)
+	{
+		tokenizer_len = _strlen(tokenizer);
+		ret_token = malloc(sizeof(char) * (tokenizer_len + _strlen(cmd) + 2));
+		if (ret_token == NULL)
+		{
+			free_double_pointer(argv, argc);
+			return (NULL);
+		}
 
-        return_token = _strcpy(return_token, tokenizer);
-        return_token = _strcat(return_token, "/");
-        return_token = _strcat(return_token, cmd);
+		ret_token = _strcpy(ret_token, tokenizer);
+		ret_token = _strcat(ret_token, "/");
+		ret_token = _strcat(ret_token, cmd);
 
-        if (access(return_token, X_OK) == 0)
-        {
-            return return_token;
-        }
-        else
-        {
-            return_token = NULL;
-        }
+		if (access(ret_token, X_OK) == 0)
+		{
+			return (ret_token);
+		}
+		else
+		{
+			ret_token = NULL;
+		}
 
-        tokenizer = strtok(NULL, ":");
-    }
-    return return_token;
+		tokenizer = strtok(NULL, ":");
+	}
+	return (ret_token);
 }
--- s_sh.c
+++ s_sh.c
@@ -2,47 +2,43 @@
 
 /**
  * prompt - executes interactive prompt
- *
+ * @interactive_str: String interactive
+ * @line: line
  * Return: 0 to success.
  */
 void *prompt(char *interactive_str, char *line)
 {
-    char **argument_variable = NULL;
-    int len_characters = 0, argument_count = 0;
-    size_t line_size = 0;
+	char **arg_var = NULL;
+	int len_chars = 0, arg_count = 0;
+	size_t line_size = 0;
 
-    while (EOF)
-    {
-        print_line(interactive_str);
+	while (EOF)
+	{
+		print_line(interactive_str);
 
-        
-        fflush(stdin);
-        
-        len_characters = getline(&line, &line_size, stdin);
+		fflush(stdin);
 
-     
-        if (line == NULL)
-            perror("Error");
+		len_chars = getline(&line, &line_size, stdin);
 
+		if (line == NULL)
+			perror("Error");
 
-        if (len_characters == EOF)
-        {
-            free(line);
-            fflush(stdin);
-            exit(127);
-        }
-        
-        line = clean_line(line);
+		if (len_chars == EOF)
+		{
+			free(line);
+			fflush(stdin);
+			exit(127);
+		}
 
-        
-        argument_count = argc(line);
-        
-        argument_variable = argv(line, argument_count);
-        if (argument_count != 0)
-        {
-            argument_variable[0] = create_path_extension(argument_variable[0], argument_variable, argument_count);
-            printf("%s\n", argument_variable[0]);
-        }        
-    }
-        
+		line = clean_line(line);
+
+		arg_count = argc(line);
+
+		arg_var = argv(line, arg_count);
+		if (arg_count != 0)
+		{
+			arg_var[0] = create_path_extension(arg_var[0], arg_var, arg_count);
+			printf("%s\n", argument_variable[0]);
+		}
+	}
 }
